659619971c5dc2ba19b72387397f6efbb057ef18
Ladauser <sovyhka29@gmail.com>
2022-01-30 12:41:05 +0300

    fixed constants

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -6,8 +6,8 @@ public interface Constants {
     String BASE_URL = "https://kitty25.testrail.io/index.php?";
     String AUTH_LOGIN = BASE_URL + "/auth/login";
     String DASHBOARD_URL = BASE_URL + "/dashboard";
-    String EMAIL = System.getenv().getOrDefault(System.getProperty("email"), PropertyReader.getProperty("email"));
-    String PASSWORD = System.getenv().getOrDefault(System.getProperty("password"), PropertyReader.getProperty("password"));
-    String INVALID_EMAIL = System.getenv().getOrDefault(System.getProperty("invalidEmail"), PropertyReader.getProperty("invalidEmail"));
-    String INVALID_PASSWORD = System.getenv().getOrDefault(System.getProperty("invalidPassword"), PropertyReader.getProperty("invalidPassword"));
+    String EMAIL = System.getenv().getOrDefault("vEmail", PropertyReader.getProperty("email"));
+    String PASSWORD = System.getenv().getOrDefault("vPassword", PropertyReader.getProperty("password"));
+    String INVALID_EMAIL = System.getenv().getOrDefault("inEmail", PropertyReader.getProperty("invalidEmail"));
+    String INVALID_PASSWORD = System.getenv().getOrDefault("inPassword", PropertyReader.getProperty("invalidPassword"));
 }
